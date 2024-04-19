31237eee5d8608e2f9959629f4ae07ace3e39898
Alejandras22310228 <160083021+Alejandras22310228@users.noreply.github.com>
2024-04-19 01:59:29 +0000

    version 2

---
+++ src/tui_test.cpp
@@ -0,0 +1,44 @@
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/screen.hpp>
+#include <ftxui/screen/color.hpp>
+#include <thread>
+#include <iostream>
+#include <string>
+
+using namespace std;
+using namespace ftxui;
+
+int main(int argc, char const *argv[])
+{
+    int fotograma = 0;
+    string reset;
+
+    while (true)
+    {
+        fotograma++;
+        Decorator cfondo = bgcolor(Color::GrayDark);
+        Decorator ctexto = color(Color::White);
+        
+
+        Element personaje = spinner(21, fotograma);
+        Element dibujo = border({
+
+            hbox(personaje) | cfondo | ctexto
+
+        });
+
+        Dimensions Alto = Dimension::Fixed(10);
+        Dimensions Ancho = Dimension::Full();
+
+        Screen pantalla = Screen::Create(Ancho, Alto); 
+
+        Render(pantalla, dibujo);
+
+        pantalla.Print();
+        reset = pantalla.ResetPosition();
+        cout << reset;
+        this_thread::sleep_for(0.1s);
+    }
+
+    return 0;
+}
